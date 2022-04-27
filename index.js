95936104e41fd975965071ce0f64d532cd30af2e
bycarlos28 <carlosfernandez28correa@gmail.com>
2022-04-27 18:37:31 +0200

    base

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import path from 'path';
-import {consulta} from './public/js/conector.js';
+import {consulta} from './public/javascripts/conector.js';
 const app = express()
 const port = 3000
 const __dirname = path.resolve();
 
 app.use('/static', express.static(__dirname + '/public'));
+app.use('/js', express.static(path.join(__dirname, 'public/javascripts')))
+app.use('/css', express.static(path.join(__dirname, 'public/stylesheets')))
+app.use('/img', express.static(path.join(__dirname, 'public/images')))
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
