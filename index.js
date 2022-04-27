0cff2298e16aad73077d098b10218a97ecbe0049
bycarlos28 <carlosfernandez28correa@gmail.com>
2022-04-27 18:09:26 +0200

    mysql conector

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const express = require('express')
+import express from 'express';
+import path from 'path';
+import {consulta} from './public/js/conector.js';
 const app = express()
 const port = 3000
+const __dirname = path.resolve();
 
 app.use('/static', express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('home')
+})
+
+app.get('/consulta/:query', (req,res) =>{
+    let sql = req.params.query;
+    consulta(sql,(err,result) => {
+        if(err) throw err
+        res.json(result)
+    })
 })
 
 app.listen(port, () => {
