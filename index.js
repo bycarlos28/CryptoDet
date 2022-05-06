7274ed8e0b7720d0847dd9fd366cd2ea5f06d276
bycarlos28 <carlosfernandez28correa@gmail.com>
2022-05-06 19:32:59 +0200

    arreglando

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import {encriptar,desencriptar} from './public/javascripts/encriptar.js'
 import {consulta} from './public/javascripts/mysqlConector.js';
 
 const app = express();
+const port = process.env.PORT
 const __dirname = path.resolve();
 
 async function getAbbreviation(){
@@ -185,6 +186,6 @@ app.get('/CryptoDetApi/:url', (req, res) => {
     console.log(conection(url))
 })*/
 
-app.listen($PORT, () => {
+app.listen(port, () => {
   //console.log(`Example app listening on port ${port}`)
 })
