c6d5af9d088f23939384d1e39532f8b3de9ba35f
bycarlos28 <carlosfernandez28correa@gmail.com>
2022-05-08 17:24:37 +0200

    arreglando

--- public/javascripts/mysqlConector.js
+++ public/javascripts/mysqlConector.js
@@ -2,6 +2,7 @@ import mysql from 'mysql2/promise.js'
 async function consulta(query) {
     const connection = await mysql.createConnection({host: process.env.HOST_DB, user: process.env.USER_DB, password: process.env.PASSWORD_DB, database: process.env.DATABASE_DB});
     const [rows, fields] = await connection.execute(query);
+    connection.end()
     return rows
 }
 
