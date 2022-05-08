4e2a133c9655ff145e24ffbcbadb54d5f842289b
bycarlos28 <carlosfernandez28correa@gmail.com>
2022-05-08 15:18:30 +0200

    arreglando

--- index.js
+++ index.js
@@ -187,5 +187,5 @@ app.get('/CryptoDetApi/:url', (req, res) => {
 })*/
 
 app.listen(port, () => {
-  //console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
