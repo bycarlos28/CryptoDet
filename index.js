bae50f096ac5450ee241627a9babb4c6ee330fe6
bycarlos28 <carlosfernandez28correa@gmail.com>
2022-05-12 14:26:05 +0200

    subiendo nueva rama

--- index.js
+++ index.js
@@ -13,19 +13,19 @@ app.use(session({secret: 'estoeslaclavesecretaparaadministarsessiones', cookie: 
 const port = process.env.PORT || 3000
 const __dirname = path.resolve();
 
-cron.schedule("*/5 * * * *",async() =>{
-    await getPrice()
-    await updatePortfolio()
-})
+// cron.schedule("*/5 * * * *",async() =>{
+//     await getPrice()
+//     await updatePortfolio()
+// })
 
-cron.schedule("*/15  * * * *",async() =>{
-    await getPrice_7days()
-    await updatePortfolio_7days()
-})
+// cron.schedule("*/15  * * * *",async() =>{
+//     await getPrice_7days()
+//     await updatePortfolio_7days()
+// })
 
-cron.schedule("*/720 * * * *",async() =>{
-    await getDataCoin()
-})
+// cron.schedule("*/720 * * * *",async() =>{
+//     await getDataCoin()
+// })
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/static', express.static(__dirname + '/public'));
@@ -49,8 +49,31 @@ var auth_destroy = function(req, res, next) {
     }
 };
 
-app.get('/',(req, res) => {
-    res.render('home')
+app.get('/', async (req, res) => {
+    let coins = {}
+    try {
+        const query = 'SELECT * FROM coins'
+        await axios.get('http://localhost:3001/consulta/'+query).then(response => {
+            coins=response.data
+        })
+    } catch (err) {
+        console.error(err);
+    }
+    let favourites = {}
+    let favourites_ids = []
+    try {
+        const query = 'SELECT * FROM favourites WHERE user_id=1 = '+1+';' // TODO: Cambiar el id de alumno por el obtenido desde session
+        await axios.get('http://localhost:3001/consulta/'+query).then(response => {
+            favourites=response.data
+        })
+        favourites.forEach(function(favourite){
+            favourites_ids.push(favourite.coin_id)
+        })
+    }
+    catch (err) {
+        console.error(err);
+    }
+    res.render('home', {coins, favourites, favourites_ids})
 })
 
 app.get('/login',(req, res) => {
@@ -114,6 +137,177 @@ app.get('/logout',auth_destroy,(req, res) => {
     res.redirect('/login')
 })
 
+app.get('/createportfolio', (req, res) => {
+    res.render('newPortfolio')
+})
+
+app.get('/addAsset/:portfolio_id/:coin_id?', async (req, res) => {
+    const default_coin = req.params.coin_id ? req.params.coin_id : 1
+    const portfolio_id = req.params.portfolio_id
+    let coins = {}
+    try {
+        const query = 'SELECT * FROM coins'
+        await axios.get('http://localhost:3001/consulta/'+query).then(response => {
+            coins=response.data
+        })
+    }
+    catch (err) {
+        console.error(err);
+    }
+    res.render('addAsset', {portfolio_id, coins, default_coin})
+})
+
+app.get('/deleteportfolio/:id', async (req, res) => {
+    let portfolio = {}
+    try {
+        const query = 'SELECT * FROM portfolios WHERE portfolio_id='+req.params.id+';'
+        await axios.get('http://localhost:3001/consulta/'+query).then(response => {
+            portfolio=response.data[0]
+        })
+    }
+    catch (err) {
+        console.error(err);
+    }
+    res.render('deletePortfolio',{portfolio})
+})
+
+app.get('/confirmdelete/:toDelete/:id', async (req, res) => {
+    const toDelete = req.params.toDelete
+    let dataToDelete
+    let query
+    if (toDelete == "portfolio") {
+        query = 'SELECT * FROM portfolios WHERE portfolio_id='+req.params.id+';'
+    } else if (toDelete == "asset") {
+        query = 'SELECT * FROM portfolios p INNER JOIN assets a ON p.portfolio_id = a.portfolio_id INNER JOIN coins c ON a.coin_id = c.coin_id WHERE a.coin_id='+req.params.id+';'
+    } else if (toDelete == "transaction") {
+        query = 'SELECT * FROM portfolios p INNER JOIN transactions t ON p.portfolio_id = t.portfolio_id INNER JOIN coins c ON t.coin_id = c.coin_id WHERE t.transaction_id='+req.params.id+';'
+    }
+    try {
+        await axios.get('http://localhost:3001/consulta/'+query).then(response => {
+            dataToDelete=response.data[0]
+        })
+    }
+    catch (err) {
+        console.error(err);
+    }
+    res.render('confirmDelete',{dataToDelete, toDelete})
+})
+
+app.get('/coin/:abb', async (req, res) => {
+    // Getting the coin from url
+    let coin = {}
+    const abbreviation=req.params.abb.toUpperCase()
+    try {
+        const query = 'SELECT * FROM coins WHERE abbreviation="'+abbreviation+'";'
+        await axios.get('http://localhost:3001/consulta/'+query).then(response => {
+            coin=response.data[0]
+        })
+    }
+    catch (err) {
+        console.error(err);
+    }
+
+    // Getting coin's contracts
+    let contracts = {}
+    try {
+        const query = 'SELECT ct.platform, ct.token_address FROM coins c INNER JOIN contracts ct ON c.coin_id = ct.coin_id WHERE c.abbreviation="'+abbreviation+'";'
+        await axios.get('http://localhost:3001/consulta/'+query).then(response => {
+            contracts=response.data
+        })
+    }
+    catch (err) {
+        console.error(err);
+    }
+
+    // Getting coin's social links
+    let socials = {}
+    try {
+        const query = 'SELECT s.type, s.platform, s.url FROM coins c INNER JOIN socials s ON c.coin_id = s.coin_id WHERE c.abbreviation="'+abbreviation+'";'
+        await axios.get('http://localhost:3001/consulta/'+query).then(response => {
+            socials=response.data
+        })
+    }
+    catch (err) {
+        console.error(err);
+    }
+
+    // Getting user's favourite coins
+    let favourites = {}
+    try {
+        const query = 'SELECT f.favourites_id, f.user_id, f.coin_id, c.name, c.abbreviation FROM favourites f INNER JOIN coins c ON f.coin_id = c.coin_id WHERE f.user_id='+1+';' // TODO: Cambiar el id de alumno por el obtenido desde session
+        await axios.get('http://localhost:3001/consulta/'+query).then(response => {
+            favourites=response.data
+        })
+    }
+    catch (err) {
+        console.error(err);
+    }
+
+    let isfavourite = false
+    let favourite_id
+    favourites.forEach(favourite => {
+        if (coin.coin_id == favourite.coin_id) {
+            isfavourite = true
+            favourite_id = favourite.favourites_id
+        }
+    });
+    res.render('coin', {coin, contracts, socials, favourites, isfavourite, favourite_id})
+})
+
+app.get('/portfolio/:id', async (req, res) => {
+    // All user's portfolios to print in the left list
+    const portfolio_id = req.params.id
+    let portfolios = {}
+    try {
+        const query = 'SELECT * from portfolios where user_id = '+1+';' // TODO: Cambiar el id de alumno por el obtenido desde session
+        await axios.get('http://localhost:3001/consulta/'+query).then(response => {
+            portfolios=response.data
+        })
+    }
+    catch (err) {
+        console.error(err);
+    }
+    
+    // The portfolio we want to show
+    let portfolio = {}
+    try {
+        const query = 'SELECT * from portfolios where portfolio_id = "'+req.params.id+'";'
+        await axios.get('http://localhost:3001/consulta/'+query).then(response => {
+            portfolio=response.data[0]
+
+        })
+    }
+    catch (err) {
+        console.error(err);
+    }
+    
+    // Getting user's favourite coins
+    let favourites = {}
+    try {
+        const query = 'SELECT f.favourites_id, f.user_id, f.coin_id, c.name, c.abbreviation FROM favourites f INNER JOIN coins c ON f.coin_id = c.coin_id WHERE f.user_id='+1+';' // TODO: Cambiar el id de alumno por el obtenido desde session
+        await axios.get('http://localhost:3001/consulta/'+query).then(response => {
+            favourites=response.data
+        })
+    }
+    catch (err) {
+        console.error(err);
+    }
+
+    // Getting the assets from the portfolio we wanna show
+    let assets = {}
+    try {
+        const query = 'SELECT c.coin_id, c.name, c.abbreviation, a.amount, a.spended FROM assets a INNER JOIN coins c ON a.coin_id=c.coin_id INNER JOIN portfolios p ON a.portfolio_id=p.portfolio_id WHERE p.user_id='+1+' and p.portfolio_id='+portfolio_id+';' // TODO: Cambiar el id de alumno por el obtenido desde session and ADD columns price and profit/loss
+        await axios.get('http://localhost:3001/consulta/'+query).then(response => {
+            assets=response.data
+        })
+    }
+    catch (err) {
+        console.error(err);
+    }
+
+    res.render('portfolio', {portfolios, portfolio, favourites, assets, portfolio_id})
+})
+
 app.get('/consulta/:query', (req,res) =>{
     let sql = req.params.query;
     consulta(sql,(err,result) => {
@@ -121,11 +315,11 @@ app.get('/consulta/:query', (req,res) =>{
         res.json(result)
     })
 })
-// app.get('/CryptoDetApi/:url', (req, res) => {
-//     let url = req.params.url
-//     console.log(url)
-//     console.log(conection(url))
-// })
+app.get('/CryptoDetApi/:url', (req, res) => {
+    let url = req.params.url
+    console.log(url)
+    console.log(conection(url))
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
-    function validar_passwords(){
-        let password = $('#password').val();
-        let password2 = $('#password2').val();
-        return password == password2
-    }
-
     $('#enviar').click(function(event) {
         if (validar_passwords() == true){
             console.log('hola')
@@ -13,4 +7,282 @@ $(document).ready(function() {
             event.preventDefault()
         }
       });
+
+    // Do a query in coin that return list of <name (abbreviation)>s    
+    const query = 'SELECT name, abbreviation FROM coins;'
+    var availableTags = []
+    $.ajax({
+        url: "http://localhost:3001/consulta/"+query,
+        method: "GET"
+    }).done(function(data){
+        for(let i=0; i != data.length; i++){
+            availableTags.push(data[i]["name"] + " (" + data[i]["abbreviation"] + ")")
+        }
+        })
+    $( "#input_searcher" ).autocomplete({
+        autoFocus: true,
+        source: availableTags,
+        change: function( event, ui ) {
+            try{
+                let value = ui.item.value.split("(")[1].slice(0,-1).toLowerCase()
+                $("#searcher").attr('action',"../coin/"+value);
+            } catch (err) {}
+        },
+        select: function( event, ui ) {
+            try{
+                value =ui.item.value.split("(")[1].slice(0,-1).toLowerCase()
+                $("#searcher").attr('action',"../coin/"+value);
+            } catch (err) {}
+        }
+    });
+    $( "#input_searcher" ).on("input", function() {
+        try{
+            value =$("#input_searcher").val().split("(")[1].slice(0,-1).toLowerCase()
+            $("#searcher").attr('action',"../coin/"+value);
+        } catch (err) {}
+     });
 });
+function validar_passwords(){
+    let password = $('#password').val();
+    let password2 = $('#password2').val();
+    return password == password2
+}
+function copyElementText(id) {
+    var text = document.getElementById(id).innerText;
+    var elem = document.createElement("textarea");
+    document.body.appendChild(elem);
+    elem.value = text;
+    elem.select();
+    document.execCommand("copy");
+    document.body.removeChild(elem);
+}
+
+async function deletefav(favid, currentPage) {
+    const id = parseInt(favid)
+    const tagId = '#fav_'+id.toString()
+    let coin_id
+    let query = 'SELECT * FROM favourites WHERE favourites_id='+id+';' //TODO insertar user_id y no "1"
+    await $.ajax({
+        url: "http://localhost:3001/consulta/"+query,
+        method: "GET",
+        success: function(result){
+            coin_id = result[0].coin_id;
+          }
+    });
+    query = 'DELETE FROM favourites WHERE favourites_id='+id+';'
+    await $.ajax({
+        url: "http://localhost:3001/consulta/"+query,
+        method: "GET"
+    });
+    $(tagId).remove()
+    if (currentPage) {
+        try {
+            $("#favstar").attr("src", "/img/emptystar.png")
+            $("#buttonfavstar").attr("onclick", "addfav('" + coin_id + "')")
+        } catch (err) {}
+    }
+}
+
+async function addfav(favid) {
+    const id = parseInt(favid)
+    let coin_id
+    let coin_name
+    let coin_abbreviation
+    let favourite_id
+    let query = 'INSERT INTO favourites (user_id, coin_id) VALUES ('+1+','+id+');' //TODO insertar user_id y no "1"
+    await $.ajax({
+        url: "http://localhost:3001/consulta/"+query,
+        method: "GET",
+        success: function(result){
+            favourite_id = result.insertId; //this will alert you the last_id
+          }
+    });
+    const tagId = 'fav_'+favourite_id.toString()
+    query = 'SELECT * FROM coins WHERE coin_id = '+id+';'
+    await $.ajax({
+        url: "http://localhost:3001/consulta/"+query,
+        method: "GET"
+    }).done(function(data){
+        coin_id = data[0].coin_id
+        coin_name = data[0].name
+        coin_abbreviation = data[0].abbreviation
+    });
+
+    // Creating the new row on favourites's table
+    const image = $("<img>").attr("src", "/img/fullstar.png").attr("class", "max-w-[15px] max-h-[15px]")
+    const button = $("<button>").attr("onclick", "deletefav('"+favourite_id+"', true)").append(image)
+    const td1= $("<td>").attr("class", "text-center").append(button)
+    const td2 = $("<td>").append($("<a>").attr("href", "../coin/"+coin_abbreviation.toLowerCase()).append($("<p>").text(coin_name + " ($" + coin_abbreviation + ")")))
+    const tr = $("<tr>").attr("id", tagId).append(td1).append(td2)
+    $("#favtable").append(tr)
+    try {
+        $("#favstar").attr("src", "/img/fullstar.png")
+        $("#buttonfavstar").attr("onclick", "deletefav('"+favourite_id+"', true)")
+    } catch (err) {}
+}
+
+async function createPortfolio() {
+    const newname= $("#newPortfolioName").val()
+    let portfolio_id
+    const query = 'INSERT INTO portfolios (user_id, name) VALUES ('+1+',"'+newname+'");' //TODO insertar user_id y no "1"
+    await $.ajax({
+        url: "http://localhost:3001/consulta/"+query,
+        method: "GET",
+        success: function(result){
+            portfolio_id = result.insertId; //this will alert you the last_id
+          }
+    });
+    window.location="../portfolio/"+portfolio_id
+}
+
+async function deletePortfolio(portfolio_id) {
+    const id = parseInt(portfolio_id)
+    let redirectedPortfolio
+    let query = 'DELETE FROM portfolios WHERE portfolio_id='+id+';'
+    await $.ajax({
+        url: "http://localhost:3001/consulta/"+query,
+        method: "GET",
+    });
+    query = 'SELECT portfolio_id FROM portfolios WHERE user_id ='+1+';' //TODO insertar user_id y no "1"
+    await $.ajax({
+        url: "http://localhost:3001/consulta/"+query,
+        method: "GET"
+    }).done(function(data){
+        redirectedPortfolio = data[0].portfolio_id
+    });
+    window.location="../portfolio/"+redirectedPortfolio
+}
+
+async function addAsset(id){
+    const portfolio_id = parseInt(id)
+    const coin_id = parseInt($('#input_asset').val())
+    const quantity = parseFloat($('#asset_quantity').val())
+    const pxc = parseFloat($('#asset_pxc').val())
+    const spended = quantity*pxc
+    const tx_timestamp = $('#transaction_timestamp').val()
+    const tx_type = document.querySelector('input[name="transaction_type"]:checked').value
+    let valid_tx = false
+
+    query = 'SELECT * FROM assets WHERE portfolio_id='+id+' and coin_id='+coin_id+''
+    await $.ajax({
+        url: "http://localhost:3001/consulta/"+query,
+        method: "GET",
+        success: async function(result){
+            if (tx_type == "buy") {
+                if (result.length == 0) {
+                    query = 'INSERT INTO assets (portfolio_id, coin_id, amount, spended) VALUES ('+ portfolio_id + ', ' + coin_id + ', ' + quantity + ", " + spended + ");"
+                    await $.ajax({
+                        url: "http://localhost:3001/consulta/"+query,
+                        method: "GET",
+                    });
+                } else {
+                    totalAmount = result[0].amount + quantity
+                    totalSpended = result[0].spended + spended
+                    query = 'UPDATE assets SET amount='+totalAmount+', spended='+totalSpended+' WHERE assets_id='+result[0].assets_id+';'
+                    await $.ajax({
+                        url: "http://localhost:3001/consulta/"+query,
+                        method: "GET",
+                    });
+                }
+                valid_tx = true
+            } else {
+                if (result.length == 0 || result[0].amount < quantity) {
+                    query = 'SELECT name, abbreviation FROM coins WHERE coin_id='+coin_id+';'
+                    await $.ajax({
+                        url: "http://localhost:3001/consulta/"+query,
+                        method: "GET",
+                        success: function(result){
+                            coin_name = result[0].name;
+                            coin_abb = result[0].abbreviation
+                        }
+                    });
+                    if (result.length == 0) {
+                        alert("No tienes ningún "+coin_name+" ($" + coin_abb +") para vender!")
+                    } else {
+                        alert("Estás intentando vender más "+coin_name+" ($" + coin_abb +") de los que tienes!")
+                    }
+                } else {
+                    totalAmount = result[0].amount - quantity
+                    totalSpended = result[0].spended - spended
+                    query = 'UPDATE assets SET amount='+totalAmount+', spended='+totalSpended+' WHERE assets_id='+result[0].assets_id+';'
+                    await $.ajax({
+                        url: "http://localhost:3001/consulta/"+query,
+                        method: "GET",
+                    });
+                    valid_tx = true
+                }
+            }
+            if (valid_tx == true) {
+                query = 'INSERT INTO transactions (type, portfolio_id, coin_id, asset_price, tx_date, tx_amount) VALUES ("'+tx_type+'", '+ portfolio_id + ', '+coin_id+','+pxc+',"'+tx_timestamp+'",'+quantity+');'
+                await $.ajax({
+                    url: "http://localhost:3001/consulta/"+query,
+                    method: "GET",
+                    success: function(result){
+                        alert("Transaction ID: " + result.insertId)
+                        window.location="../portfolio/"+portfolio_id
+                    }
+                });
+            }
+        }
+    });
+
+}
+
+async function deleteAsset(portfolio_id, asset_id) {
+    query = 'DELETE FROM assets WHERE assets_id='+asset_id+';'
+    await $.ajax({
+        url: "http://localhost:3001/consulta/"+query,
+        method: "GET",
+        success: function(result){
+            alert("Asset deleted successfully")
+            window.location="../portfolio/"+portfolio_id
+        }
+    });
+}
+
+async function deleteTransaction(portfolio_id, coin_id, transaction_id, tx_type) {
+    let transaction_data
+    let asset_data
+    query = 'SELECT * FROM transactions WHERE transaction_id='+transaction_id+';'
+    await $.ajax({
+        url: "http://localhost:3001/consulta/"+query,
+        method: "GET",
+        success: function(result){
+            transaction_data=result[0]
+        }
+    });
+    query = 'SELECT * FROM assets WHERE portfolio_id='+portfolio_id+' and coin_id='+coin_id+';'
+    await $.ajax({
+        url: "http://localhost:3001/consulta/"+query,
+        method: "GET",
+        success: function(result){
+            asset_data=result[0]
+        }
+    });
+    let totalAmount
+    let totalSpended
+    if (tx_type == "buy") {
+        totalAmount = asset_data.amount-(transaction_data.tx_amount)
+        totalSpended = asset_data.spended-(transaction_data.tx_amount*transaction_data.asset_price)
+    } else if (tx_type == "sell") {
+        totalAmount = asset_data.amount+(transaction_data.tx_amount)
+        totalSpended = asset_data.spended+(transaction_data.tx_amount*transaction_data.asset_price)
+    }
+    query = 'UPDATE assets SET amount='+totalAmount+', spended='+totalSpended+' WHERE portfolio_id='+portfolio_id+' and coin_id='+coin_id+';'
+    await $.ajax({
+        url: "http://localhost:3001/consulta/"+query,
+        method: "GET",
+        success: function(result){
+            alert("Asset updated successfully")
+        }
+    });
+    query = 'DELETE FROM transactions WHERE transaction_id='+transaction_id+';'
+    await $.ajax({
+        url: "http://localhost:3001/consulta/"+query,
+        method: "GET",
+        success: function(result){
+            alert("Transaction deleted successfully")
+            window.location="../portfolio/"+portfolio_id // better delete the row and not redirect the url
+        }
+    });
+}
