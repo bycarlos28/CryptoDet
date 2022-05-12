41821a7b475753258103a7c695b8807196ad6c49
bycarlos28 <carlosfernandez28correa@gmail.com>
2022-05-12 13:40:50 +0200

    subiendo heroku

--- index.js
+++ index.js
@@ -13,19 +13,19 @@ app.use(session({secret: 'estoeslaclavesecretaparaadministarsessiones', cookie: 
 const port = process.env.PORT || 3000
 const __dirname = path.resolve();
 
-// cron.schedule("*/5 * * * *",async() =>{
-//     await getPrice()
-//     await updatePortfolio()
-// })
+cron.schedule("*/5 * * * *",async() =>{
+    await getPrice()
+    await updatePortfolio()
+})
 
-// cron.schedule("*/15  * * * *",async() =>{
-//     await getPrice_7days()
-//     await updatePortfolio_7days()
-// })
+cron.schedule("*/15  * * * *",async() =>{
+    await getPrice_7days()
+    await updatePortfolio_7days()
+})
 
-// cron.schedule("*/720 * * * *",async() =>{
-//     await getDataCoin()
-// })
+cron.schedule("*/720 * * * *",async() =>{
+    await getDataCoin()
+})
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/static', express.static(__dirname + '/public'));
